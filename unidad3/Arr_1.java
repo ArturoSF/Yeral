71a403e9bf56408746970b89567856c02bfd1506
yeraldin cano <yeral-090796@hotmail.com>
2014-11-23 03:14:56 -0600

    no message

---
+++ unidad3/Arr_1.java
@@ -0,0 +1,18 @@
+package unidad3;
+
+public class Arr_1 
+{
+
+	public static void main(String[] args) 
+	{
+		int arreglo []= {0,1,2,3,4};
+		for (int i=0;i<arreglo.length;i++)
+		{
+			System.out.println("Posicion:"+i+ "Itelemento:"+arreglo[i]);
+		}
+		
+		
+
+	}
+
+}
---
+++ unidad3/Mayor_menor.java
@@ -0,0 +1,17 @@
+package unidad3;
+
+public class Mayor_menor {
+
+	public static void main(String[] args)
+	{
+		int x=5,y=10;
+		if(x==y)		
+		System.out.println("x es menor que y");
+		System.out.println("x es mayor que y");
+		
+		
+		
+
+	}
+
+}
